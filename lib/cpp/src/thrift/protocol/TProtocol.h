fdeb59537f176a16dbc056aba031565d6d69914d
kalashnik <kalashnik@apriorit.com>
2014-08-01 16:05:08 +0300

    Applied own patches. Added patch files.

--- lib/cpp/src/thrift/protocol/TProtocol.h
+++ lib/cpp/src/thrift/protocol/TProtocol.h
@@ -34,6 +34,9 @@
 #include <map>
 #include <vector>
 
+#ifdef HAVE_SYS_PARAM_H
+#include <sys/param.h>
+#endif
 
 // Use this to get around strict aliasing rules.
 // For example, uint64_t i = bitwise_cast<uint64_t>(returns_double());
@@ -73,15 +76,6 @@ static inline To bitwise_cast(From from) {
   u.f = from;
   return u.t;
 }
-
-
-namespace apache { namespace thrift { namespace protocol {
-
-using apache::thrift::transport::TTransport;
-
-#ifdef HAVE_SYS_PARAM_H
-#include <sys/param.h>
-#endif
 
 #ifndef __THRIFT_BYTE_ORDER
 # if defined(BYTE_ORDER) && defined(LITTLE_ENDIAN) && defined(BIG_ENDIAN)
@@ -139,6 +133,10 @@ using apache::thrift::transport::TTransport;
 #else /* __THRIFT_BYTE_ORDER */
 # error "Can't define htonll or ntohll!"
 #endif
+
+namespace apache { namespace thrift { namespace protocol {
+
+using apache::thrift::transport::TTransport;
 
 /**
  * Enumerated definition of the types that the Thrift protocol supports.
